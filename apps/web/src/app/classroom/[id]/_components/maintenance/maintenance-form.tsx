a5c8dc6b0e396dac44ecc7d00e2537d9d9ff6e52
Shreyas Ayyengar <shreyas.ayyengar@gmail.com>
2026-03-11 13:07:23 -0700

    refactor(maintenance): enhance form UI with scroll area, footer, and general maintenance section

--- apps/web/src/app/classroom/[id]/_components/maintenance/maintenance-form.tsx
+++ apps/web/src/app/classroom/[id]/_components/maintenance/maintenance-form.tsx
@@ -1,5 +1,9 @@
 import { type classroomSchema, type maintenanceEntrySchema, maintenanceFormSchema } from "@redwood/contracts";
+import { Button } from "@redwood/shad-ui/components/button";
+import { DialogClose, DialogFooter } from "@redwood/shad-ui/components/dialog";
+import { ScrollArea } from "@redwood/shad-ui/components/scroll-area";
 import { Separator } from "@redwood/shad-ui/components/separator";
+import { cn } from "@redwood/shad-ui/lib/utils";
 import { createFormHook, createFormHookContexts } from "@tanstack/react-form";
 import { useMutation } from "@tanstack/react-query";
 import type { z } from "zod";
@@ -54,27 +58,51 @@ export default function MaintenanceForm({
   });
 
   return (
-    <div>
-      <div className="flex h-fit flex-col gap-5">
-        {/* Date */}
-        <form.AppField name="date">{(field) => <field.DateField existingDate={maintenanceEntry?.date} />}</form.AppField>
-        <Separator className="bg-amber-500" />
-        <form.AppField name="greenStripe">{(field) => <field.GreenStripeField existingValues={maintenanceEntry?.greenStripe} />}</form.AppField>
+    <>
+      <ScrollArea className="max-h-[calc(100dvh-300px)] rounded-2xl bg-background/40 p-3">
+        {" "}
+        <div className="my-2 flex flex-col gap-5 px-1">
+          {/* Date */}
+          <form.AppField name="date">{(field) => <field.DateField existingDate={maintenanceEntry?.date} />}</form.AppField>
+          <Separator className="bg-indigo-500" />
 
-        <Separator className="bg-amber-500" />
+          <Separator className="bg-indigo-500" />
 
-        {/*/!* General Maintenance *!/*/}
-        {/*<div className="flex flex-col items-end">*/}
-        {/*  <div className="h-full justify-center space-y-3 rounded-md bg-zinc-950/30 p-3 ring-1 ring-white/15">*/}
-        {/*    <form.AppField name="surfacesWiped">*/}
-        {/*      {(field) => <field.SurfacesWipedField existingValue={maintenanceEntry?.surfacesWiped} />}*/}
-        {/*    </form.AppField>*/}
-        {/*    <form.AppField name="equipmentChecked">*/}
-        {/*      {(field) => <field.EquipmentCheckedField existingValue={maintenanceEntry?.equipmentChecked} />}*/}
-        {/*    </form.AppField>*/}
-        {/*  </div>*/}
-        {/*</div>*/}
-      </div>
-    </div>
+          <Separator className="bg-indigo-500" />
+
+          {/* General Maintenance */}
+          <div className="flex flex-col items-start">
+            <h2 className="mb-3 font-semibold text-lg">General Maintenance</h2>
+            <div className="w-full justify-center space-y-3 rounded-md bg-zinc-950/30 p-3 ring-1 ring-white/15">
+              <form.AppField name="surfacesWiped">
+                {(field) => <field.SurfacesWipedField existingValue={maintenanceEntry?.surfacesWiped} />}
+              </form.AppField>
+              <form.AppField name="equipmentChecked">
+                {(field) => <field.EquipmentCheckedField existingValue={maintenanceEntry?.equipmentChecked} />}
+              </form.AppField>
+            </div>
+          </div>
+        </div>
+      </ScrollArea>
+      <DialogFooter className="my-3">
+        <DialogClose asChild>
+          <Button variant="outline">Cancel</Button>
+        </DialogClose>
+        <form.Subscribe selector={(state) => [state.canSubmit, state.isSubmitting]}>
+          {([canSubmit, isSubmitting]) => (
+            <Button
+              className={cn(
+                `${canSubmit ? "bg-foreground hover:cursor-pointer hover:bg-foreground/50" : "cursor-not-allowed hover:bg-accent"}`,
+                `${isSubmitting ? "cursor-wait" : "cursor-default"}`
+              )}
+              onClick={form.handleSubmit}
+              disabled={!canSubmit || isSubmitting}
+            >
+              {isSubmitting ? "Logging..." : "Log Maintenance"}
+            </Button>
+          )}
+        </form.Subscribe>
+      </DialogFooter>
+    </>
   );
 }
--- apps/web/src/app/classroom/[id]/_components/new-maintenance-dialog.tsx
+++ apps/web/src/app/classroom/[id]/_components/new-maintenance-dialog.tsx
@@ -1,6 +1,5 @@
 import type { classroomSchema, maintenanceEntrySchema } from "@redwood/contracts";
-import { Button } from "@redwood/shad-ui/components/button";
-import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@redwood/shad-ui/components/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@redwood/shad-ui/components/dialog";
 import type { z } from "zod";
 import MaintenanceForm from "./maintenance/maintenance-form";
 
@@ -17,18 +16,13 @@ export default function NewMaintenanceDialog({
     <Dialog>
       <form>
         <DialogTrigger asChild>{children}</DialogTrigger>
-        <DialogContent className="w-fit bg-zinc-800 sm:max-w-none">
+        <DialogContent className="bg-zinc-800 p-3">
           <DialogHeader>
             <DialogTitle className="mx-auto rounded-md bg-zinc-950/30 px-10 py-3 text-center text-2xl ring-1 ring-white/15">
               {maintenanceEntry ? `${maintenanceEntry.completedBy.split("@")[0]}'s` : "New"} Maintenance Log:
             </DialogTitle>
           </DialogHeader>
           <MaintenanceForm roomId={roomId} maintenanceEntry={maintenanceEntry} />
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DialogClose>
-          </DialogFooter>
         </DialogContent>
       </form>
     </Dialog>
